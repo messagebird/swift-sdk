99443ccd564b411b12b12cade42b17358dfb815b
Meena Alfons <meena.alfons@messagebird.com>
2024-01-17 09:43:23 +0100

    Capabilities + comments

--- Package.swift
+++ Package.swift
@@ -17,7 +17,9 @@ let package = Package(
             // This URL is only usable if the repo is public.
             // Until this repo goes public, you can use a local path instead.
             // Remove the `url` and `checksum` lines and uncomment the `path` line.
+            // In XCode -> File -> Packages -> Update to Latest Package Versions
             // path: "../path/to/BirdKit.xcframework.zip"
+
             url: "https://github.com/messagebird/swift-sdk/releases/download/0.0.1/BirdKit.xcframework.zip",
             checksum: "48b30d1d4e30a8c5a36c90f690eef799742ab48672676bdc7515dd217d65a5ff"
         ),
