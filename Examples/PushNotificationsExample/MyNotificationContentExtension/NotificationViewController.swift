2f7cf605202d1ab46249a476e4ce90f8d40cb3a5
Meena Alfons <meena.alfons@messagebird.com>
2024-06-24 11:55:48 +0200

    Update pushnotifications example

---
+++ Examples/PushNotificationsExample/MyNotificationContentExtension/NotificationViewController.swift
@@ -0,0 +1,11 @@
+//
+//  NotificationViewController.swift
+//  MyNotificationContentExtension
+//
+//  Created by Meena Alfons on 24/06/2024.
+//
+
+import BirdKit
+
+class NotificationViewController: BirdNotificationViewController {
+}
--- Examples/PushNotificationsExample/PushNotificationsExample/AppDelegate.swift
+++ Examples/PushNotificationsExample/PushNotificationsExample/AppDelegate.swift
@@ -5,32 +5,82 @@
 //  Created by Meena Alfons on 13/12/2023.
 //
 
+import os
 import Foundation
 import UIKit
+
 import BirdKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+    
     static var bird: Bird? = nil
+    static let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "AppDelegate")
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        AppDelegate.bird = Bird(workspaceId: "blah")
+        do {
+            Self.bird = try Bird()
+        } catch {
+            Self.logger.info("failed to create bird \(error)")
+        }
         
+        UNUserNotificationCenter.current().delegate = self
         return true
+    }
+    
+    // This method will be called from Button "Ask for notifications permission"
+    static func requestNotificationsPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]) { granted, error in
+            guard granted else {
+                self.logger.log("Notification permission is not gratned")
+                return
+            }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+
     }
     
     func application(
         _ application: UIApplication,
         didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
     ) {
+        Self.bird?.notifications.registerDevice(deviceToken: deviceToken)
         
-        print("didRegisterForRemoteNotificationsWithDeviceToken")
-        AppDelegate.bird?.notifications.registerDevice(deviceToken: deviceToken)
-        
+        Self.logger.log("APNS token: \(deviceToken.map { String(format: "%02x", $0) }.joined())")
     }
     
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        
-        print("didFailToRegisterForRemoteNotificationsWithError")
-        print(error)
+        Self.logger.log(#function)
+    }
+    
+    
+    func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+        Self.bird?.notifications.applicationDidReceiveRemoteNotification(userInfo: userInfo, completionHandler: completionHandler)
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        Self.logger.log(#function)
+        let options = Self.bird?.notifications.willPresentNotification(notification)
+        completionHandler(options ?? [.sound, .banner, .list])
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        Self.logger.log(#function)
+        Self.bird?.notifications.didReceiveResponse(response)
     }
 }
